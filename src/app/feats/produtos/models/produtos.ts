b6be5f2d0caf54dcf64545cbd4a70f8d2ff25135
Rodrigo de Souza <rodrigo.souza.thunder@gmail.com>
2026-09-01 17:04:29 -0300

    interface produtos

---
+++ src/app/feats/produtos/models/produtos.ts
@@ -0,0 +1,8 @@
+export interface Produtos {
+    "id":"string"
+    "nome":"string"
+    "descricao":"string"
+    "preco":Number
+    "peso":Number
+    "estoque":Number
+}
---
+++ src/app/feats/produtos/produtos-filtros/produtos-filtros.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-produtos-filtros',
+  styleUrl: './produtos-filtros.css',
+  templateUrl: './produtos-filtros.html',
+})
+export class ProdutosFiltros {
+}
