35b9d1cf3df8842d6f76e1792f9b4afc9f943ae0
Gregory <gregorysantos575@gmail.com>
2026-09-01 17:15:18 -0300

    adiciona pesquisar no header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormField],
   selector: 'app-header',
   styleUrl: './header.css',
   templateUrl: './header.html',
 })
-export class Header {}
+export class Header {
+
+  // protected readonly produtosService = inject(ProdutosService);
+
+  pesquisaModel = signal<string>("");
+
+  pesquisaForm = form(this.pesquisaModel);
+
+  pesquisarProduto(event: SubmitEvent) {
+    event.preventDefault()
+
+  }
+}
