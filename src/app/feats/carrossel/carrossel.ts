585c3bb7a02419c3b2c8a04416894a6275bef26a
Beatriz Haiana <ferrari.beatriz14@gmail.com>
2026-09-03 17:57:40 -0300

    edita css carrossel

--- src/app/feats/carrossel/carrossel.ts
+++ src/app/feats/carrossel/carrossel.ts
@@ -7,5 +7,46 @@ import { Component } from '@angular/core';
   templateUrl: './carrossel.html',
 })
 export class Carrossel {
+  
+  indiceAtual = 0;
 
+  produtos = [
+    {
+      categoria: 'Favoritos',
+      nome: 'Matcha Latte Gelado',
+      preco: 24.90,
+      descricao: 'Uma bebida refrescante, cremosa e cheia de energia que combina chá verde em pó com leite frio e gelo.',
+      imagem: 'assets/imagens/matcha.png'
+    },
+    {
+      categoria: 'Favoritos',
+      nome: 'Café Gelado',
+      preco: 19.90,
+      descricao: 'Uma bebida clássica adaptada para o verão brasileiro.',
+      imagem: 'assets/imagens/cafe-gelado.png'
+    }
+  ];
+
+  produto = this.produtos[this.indiceAtual];
+
+  anterior() {
+    this.indiceAtual = this.indiceAtual === 0 ? this.produtos.length - 1 : this.indiceAtual - 1;
+    this.produto = this.produtos[this.indiceAtual];
+  }
+
+  proximo() {
+    this.indiceAtual = this.indiceAtual === this.produtos.length - 1 ? 0 : this.indiceAtual + 1;
+    this.produto = this.produtos[this.indiceAtual];
+  }
+
+  irPara(indice: number) {
+    this.indiceAtual = indice;
+    this.produto = this.produtos[this.indiceAtual];
+  }
+
+  adicionar(produto: any) {
+    console.log('Produto adicionado:', produto);
+  }
 }
+
+
