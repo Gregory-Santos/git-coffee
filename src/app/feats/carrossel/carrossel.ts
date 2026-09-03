5752f8d1273befa9623748a1ce33c2480a2c91bd
Gregory <gregorysantos575@gmail.com>
2026-09-03 14:43:25 -0300

    cria carrossel

---
+++ src/app/feats/carrossel/carrossel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-carrossel',
+  styleUrl: './carrossel.css',
+  templateUrl: './carrossel.html',
+})
+export class Carrossel {
+
+}
--- src/app/feats/home/home.ts
+++ src/app/feats/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { Carrossel } from "../carrossel/carrossel";
 
 @Component({
-  imports: [],
+  imports: [Carrossel],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
