166487e3f22a2e799797eb06f8a0dcb424c0c7cc
Rafael Vinícius da Rocha <rafavini82@gmail.com>
2026-09-04 14:43:44 -0300

    config-carrossel
    ajustei as configurações relacionadas ao carrossel

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
+import { Carrinho } from "./feats/carrinho/carrinho";
 
 @Component({
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Carrinho],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
