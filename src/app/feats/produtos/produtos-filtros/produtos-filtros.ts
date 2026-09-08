e884d4ab8a29a8b1152b3986bc06a3df94dc3331
frontenddigo <rodrigo.souza.thunder@gmail.com>
2026-09-08 10:33:48 -0300

    logica de filtro (categorias, busca por nome e forms angular) falta css e chamar na pagina produtos.

--- src/app/feats/produtos/produtos-filtros/produtos-filtros.ts
+++ src/app/feats/produtos/produtos-filtros/produtos-filtros.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import [ FormsModule]from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [FormsModule]
   selector: 'app-produtos-filtros',
   styleUrl: './produtos-filtros.css',
   templateUrl: './produtos-filtros.html',
 })
 export class ProdutosFiltros {
+
+  filtros={
+    busca:'',
+    categoria:'',
+    precoMaximo: null as number/null,
+    ordenacao:'',
+  };
+
+  aplicarFiltros(){
+    console.log('filtros aplicados:');
+    console.log(this.filtros);
+  }
+
+  limparFiltros(){
+
+    this.filtros = {
+    busca:'',
+    categoria:'',
+    precoMaximo:null,
+    ordenacao: ''
+  };
+
+  console.log('filtros limpos');
+
+}
 }
