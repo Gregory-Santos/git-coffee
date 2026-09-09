8c087b113817c8f455cc7d983d5db0011a3fae82
Brenda Melo <brendasilvam7@gmail.com>
2026-09-09 17:10:40 -0300

    conecta-botao-produtos-com-carrinho

--- src/app/feats/produtos/produtos-card/produtos-card.ts
+++ src/app/feats/produtos/produtos-card/produtos-card.ts
@@ -1,14 +1,23 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { Produto } from '../models/produto';
 import { PRODUTOS_API } from '../models/produtos-api/produtos-api';
+import { CarrinhoService } from '../../carrinho/carrinho-service';
 
 @Component({
   selector: 'app-produtos-card',
   templateUrl: './produtos-card.html',
-  styleUrl: './produtos-card.css'
+  styleUrl: './produtos-card.css',
 })
 export class ProdutosCard {
+ adicionado = signal(false);
+  
+  //injetando carrinho service//
+  constructor(private CarrinhoService: CarrinhoService) {}
+  adicionarProduto() {
+    this.CarrinhoService.adicionarProduto(this.produto());
+    
+this.adicionado.set(true)
+   }
 
   produto = input.required<Produto>();
-
 }
