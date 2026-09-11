a4395fd9fdab379fd7727e851287fcefe991f278
Beatriz Haiana <ferrari.beatriz14@gmail.com>
2026-09-11 14:51:53 -0300

    aprimora descrição

--- src/app/feats/produtos/models/produtos-api/produtos-api.ts
+++ src/app/feats/produtos/models/produtos-api/produtos-api.ts
@@ -3,93 +3,82 @@ import { Produto } from '../produto';
 export const PRODUTOS_API: Produto[] = [
 
   {
-    id: 1,
-    nome: 'Strawberry Matcha Latte Gelado',
-    descricao: '[bebida-gelada] Ingredientes: Chá verde matcha, purê de morango natural, leite, gelo e pedaços de morango.',
-    preco: 28.90,
-   
-    img: 'img/strawberry-matcha-latte-gelado.png'
-  },
+  id: 1,
+  nome: 'Strawberry Matcha Latte Gelado',
+  descricao: 'Ingredientes: Chá verde matcha, purê de morango natural, leite, gelo e pedaços de morango. Uma combinação de matcha e morango com leite, servida bem gelada [bebida-gelada].',
+  preco: 28.90,
+  img: 'img/strawberry-matcha-latte-gelado.png'
+},
 
-  {
-    id: 2,
-    nome: 'Pink Drink de Morango',
-    descricao: '[bebida-gelada] Ingredientes: Leite vegetal, calda e purê de morango, chantilly e pedaços de morango.',
-    preco: 26.90,
-    
-    img: 'img/pink-drink-de-morango.png'
-  },
+{
+  id: 2,
+  nome: 'Pink Drink de Morango',
+  descricao: 'Ingredientes: Leite vegetal, calda e purê de morango, chantilly e pedaços de morango. Uma combinação cremosa de morango e leite vegetal, finalizada com chantilly [bebida-gelada].',
+  preco: 26.90,
+  img: 'img/pink-drink-de-morango.png'
+},
 
-  {
-    id: 3,
-    nome: 'Iced Mocha Cremoso',
-    descricao: '[bebida-gelada] Ingredientes: Espresso, calda de chocolate, leite, gelo, chantilly e cacau em pó.',
-    preco: 27.90,
-   
-    img: 'img/iced-mocha-cremoso.png'
-  },
+{
+  id: 3,
+  nome: 'Iced Mocha Cremoso',
+  descricao: 'Ingredientes: Espresso, calda de chocolate, leite, gelo, chantilly e cacau em pó. Café espresso com chocolate e leite, servido gelado e finalizado com chantilly [bebida-gelada].',
+  preco: 27.90,
+  img: 'img/iced-mocha-cremoso.png'
+},
 
-  {
-    id: 4,
-    nome: 'Iced Americano',
-    descricao: '[bebida-gelada] Ingredientes: Dose dupla de café espresso, água filtrada e bastante gelo.',
-    preco: 19.90,
-  
-    img: 'img/iced-americano.png'
-  },
+{
+  id: 4,
+  nome: 'Iced Americano',
+  descricao: 'Ingredientes: Dose dupla de café espresso, água filtrada e bastante gelo. Uma versão gelada e direta do clássico café americano [bebida-gelada].',
+  preco: 19.90,
+  img: 'img/iced-americano.png'
+},
 
-  {
-    id: 5,
-    nome: 'Iced Cappuccino Especial',
-    descricao: '[bebida-gelada] Ingredientes: Espresso, leite gelado vaporizado, gelo, chantilly e canela em pó.',
-    preco: 25.90,
+{
+  id: 5,
+  nome: 'Iced Cappuccino Especial',
+  descricao: 'Ingredientes: Espresso, leite gelado vaporizado, gelo, chantilly e canela em pó. Cappuccino servido gelado, com chantilly e canela na finalização [bebida-gelada].',
+  preco: 25.90,
+  img: 'img/iced-cappuccino-especial.png'
+},
 
-    img: 'img/iced-cappuccino-especial.png'
-  },
+{
+  id: 6,
+  nome: 'Iced Matcha Latte Clássico',
+  descricao: 'Ingredientes: Chá verde matcha puro, leite e gelo. Matcha e leite combinados em uma bebida simples e refrescante [bebida-gelada].',
+  preco: 24.90,
+  img: 'img/iced-matcha-latte-classico.png'
+},
 
-  {
-    id: 6,
-    nome: 'Iced Matcha Latte Clássico',
-    descricao: '[bebida-gelada] Ingredientes: Chá verde matcha puro, leite e gelo.',
-    preco: 24.90,
-  
-    img: 'img/iced-matcha-latte-classico.png'
-  },
+{
+  id: 7,
+  nome: 'Iced Latte',
+  descricao: 'Ingredientes: Café espresso, leite gelado e cubo de gelo. Uma combinação clássica de espresso e leite, servida gelada [bebida-gelada].',
+  preco: 22.90,
+  img: 'img/iced-latte.png'
+},
 
-  {
-    id: 7,
-    nome: 'Iced Latte',
-    descricao: '[bebida-gelada] Ingredientes: Café espresso, leite gelado e cubo de gelo.',
-    preco: 22.90,
-  
-    img: 'img/iced-latte.png'
-  },
+{
+  id: 8,
+  nome: 'Cappuccino Tradicional',
+  descricao: 'Ingredientes: Café espresso, leite vaporizado com crema densa e polvilhado de cacau. Cappuccino clássico preparado com espresso, leite vaporizado e cacau [bebida-quente].',
+  preco: 21.90,
+  img: 'img/cappuccino-tradicional.png'
+},
 
-  {
-    id: 8,
-    nome: 'Cappuccino Tradicional',
-    descricao: '[bebida-quente] Ingredientes: Café espresso, leite vaporizado com crema densa e polvilhado de cacau.',
-    preco: 21.90,
-   
-    img: 'img/cappuccino-tradicional.png'
-  },
+{
+  id: 9,
+  nome: 'Caramel Macchiato Quente',
+  descricao: 'Ingredientes: Leite vaporizado, espresso, xarope de baunilha e cobertura de calda de caramelo. Espresso com leite vaporizado, baunilha e cobertura de caramelo [bebida-quente].',
+  preco: 31.90,
+  img: 'img/caramel-macchiato.png'
+},
 
-  {
-    id: 9,
-    nome: 'Caramel Macchiato Quente',
-    descricao: '[bebida-quente] Ingredientes: Leite vaporizado, espresso, xarope de baunilha e cobertura de calda de caramelo.',
-    preco: 31.90,
-  
-    img: 'img/caramel-macchiato.png'
-  },
-
-  {
-    id: 10,
-    nome: 'Café Americano Quente',
-    descricao: '[bebida-quente] Ingredientes: Café espresso diluído em água quente.',
-    preco: 19.90,
-  
-    img: 'img/cafe-americano.png'
-  }
-
+{
+  id: 10,
+  nome: 'Café Americano Quente',
+  descricao: 'Ingredientes: Café espresso diluído em água quente. Uma opção clássica e simples de espresso com água quente [bebida-quente].',
+  preco: 19.90,
+  img: 'img/cafe-americano.png'
+}
 ];
