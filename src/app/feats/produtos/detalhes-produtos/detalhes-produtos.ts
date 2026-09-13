eef1d48a7e86493fc50fc182d51e292cba0fa61d
Beatriz Haiana <ferrari.beatriz14@gmail.com>
2026-09-13 13:12:24 -0300

    adiciona popup

--- src/app/feats/produtos/detalhes-produtos/detalhes-produtos.ts
+++ src/app/feats/produtos/detalhes-produtos/detalhes-produtos.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
 import { Produto } from '../models/produto';
 import { PRODUTOS_API } from '../models/produtos-api/produtos-api';
@@ -20,9 +20,11 @@ export class DetalhesProdutos {
   private readonly location = inject(Location);
   private readonly route = inject(ActivatedRoute);
   private readonly scroller = inject(ViewportScroller);
-  private readonly CarrinhoService= inject (CarrinhoService);
-  
+  private readonly CarrinhoService = inject(CarrinhoService);
+
   protected produto: Produto | undefined;
+
+  adicionado = signal(false);
 
   constructor() {
     this.route.paramMap.subscribe(params => {
@@ -31,16 +33,22 @@ export class DetalhesProdutos {
       this.produto = PRODUTOS_API.find(
         produto => produto.id === Number(produtoId)
       );
-      
 
       this.scroller.scrollToPosition([0, 0]);
     });
   }
-    voltar() {
+
+  voltar() {
     this.location.back();
   }
 
   adicionar(produto: Produto) {
     this.CarrinhoService.adicionarProduto(produto);
+
+    this.adicionado.set(true);
+
+    setTimeout(() => {
+      this.adicionado.set(false);
+    }, 1500);
   }
 }
